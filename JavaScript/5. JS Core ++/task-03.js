f6f9e6d789ba2c1e611d89fb6ae6e32cc3277da3
Stanislav Donev <77002111+donev-stan@users.noreply.github.com>
2022-08-17 10:37:35 +0300

    task-03 done

--- JavaScript/5. JS Core ++/task-03.js
+++ JavaScript/5. JS Core ++/task-03.js
@@ -1,14 +1,13 @@
-const pentagonPoints = (sidePoints, result = 1) => {
-	if (sidePoints === 1) return result;
+const pentagonPoints = (sidePoints, result) => {
+	!result ? (result = 0) : result;
+	if (sidePoints === 1) return result + 1;
 
-	console.log(sidePoints);
+	result += sidePoints + 3 * (sidePoints - 1) + (sidePoints - 2);
 
-	// result += sidePoints * 2 +
 	return pentagonPoints(sidePoints - 1, result);
 };
 
-console.log(pentagonPoints(1)); // sum points: 1
-console.log(pentagonPoints(2)); // sum points: 1
-console.log(pentagonPoints(3)); // sum points: 6
-console.log(pentagonPoints(4)); // sum points: 16
-console.log(pentagonPoints(5)); // sum points: 31
+// console.log(pentagonPoints(1)); // sum points: 1
+// console.log(pentagonPoints(2)); // sum points: 6
+// console.log(pentagonPoints(3)); // sum points: 16
+console.log(pentagonPoints(4)); // sum points: 31
